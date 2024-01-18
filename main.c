b0d7d5e7c9a60e2a318932a720a0a7cc71a2dde9
kylie hubbard <119622216+kyliehubbard@users.noreply.github.com>
2024-01-17 20:43:49 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int lineNum = 0;
+    int wordNum = 0;
+    int charNum = 0;
+
+    int c;
+    int inWord = 0; 
+
+    while ((c = getchar()) != EOF) {
+        charNum++;
+
+        if (c == '\n' || c == ' ' || c == '\t') {
+            if (inWord) {
+                wordNum++;
+                inWord = 0;  
+            }
+            if (c == '\n') {
+                lineNum++;
+            }
+        } else {
+            inWord = 1;
+        }
+    }
+
+    if (inWord) {
+        wordNum++;
+    }
+
+    printf("Number of lines: %d\n", lineNum);
+    printf("Number of words: %d\n", wordNum);
+    printf("Number of characters: %d\n", charNum);
+
+    return 0;
+}
